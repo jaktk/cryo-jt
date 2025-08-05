f23389e07c214b1fe71e823f3c115a2a38e4ffdf
Jakub Tkaczuk <jtkaczuk@ethz.ch>
2025-08-05 09:38:23 +0200

    add sensor uncertainty to monte carlo calc

--- src/data_analysis/calculate_jt_coefficients.py
+++ src/data_analysis/calculate_jt_coefficients.py
@@ -5,59 +5,191 @@ import pandas as pd
 from scipy import optimize
 from numpy.polynomial.chebyshev import Chebyshev
 from sklearn.metrics import r2_score
-import warnings
-warnings.filterwarnings('ignore')
-
-import sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'src', 'data_analysis'))
 from FluidProps import FluidProps
 from get_git_root import get_git_root
 
 
+class CernoxCal(object):
+    serNum = ''
+
+    def __init__(self, serNum):
+        self.serNum = serNum
+        self.coefficients = self._set_coefficients()
+        self.tbl = self._set_tbl()
+        self.dat = self._set_dat()
+
+    def _set_coefficients(self):
+        fpath = os.path.join(get_git_root(os.getcwd()),
+                             'data',
+                             'cernox_calibration_data',
+                             self.serNum,
+                             f'{self.serNum}.cof')
+        c_dict = {}
+        with open(fpath, 'r') as fh:
+            coefs = []
+            limits = ''
+            while True:
+                line = fh.readline()
+                if not line:
+                    c_dict[limits] = {'Zlower': Zlower, 'Zupper': Zupper, 'coefs': coefs}
+                    break
+                l = line.strip().split()
+                if l[0] == 'Lower' and l[1] == 'Resist.':
+                    limits = l[-1]
+                elif l[0] == 'Upper' and l[1] == 'Resist.':
+                    limits = limits + ',' + l[-1]
+                elif l[0] == 'Zlower':
+                    Zlower = float(l[-1])
+                elif l[0] == 'Zupper':
+                    Zupper = float(l[-1])
+                elif list(l[0])[0] == 'C' and list(l[0])[1] == '(':
+                    coefs.append(float(l[-1]))
+                elif coefs and list(l[0])[0] != 'C':
+                    c_dict[limits] = {'Zlower': Zlower, 'Zupper': Zupper, 'coefs': coefs}
+                    coefs = []
+        return c_dict
+
+    def _set_tbl(self):
+        fpath = os.path.join(get_git_root(os.getcwd()),
+                             'data',
+                             'cernox_calibration_data',
+                             self.serNum,
+                             f'{self.serNum}.tbl')
+        return pd.read_csv(fpath, sep=r'\s+', header=[0,1])
+
+    def _set_dat(self):
+        fpath = os.path.join(get_git_root(os.getcwd()),
+                             'data',
+                             'cernox_calibration_data',
+                             self.serNum,
+                             f'{self.serNum}.dat')
+        return pd.read_csv(fpath, sep=r'\s+', header=[0,1])
+
+    def get_T_from_coefs(self, R):
+        T = 0
+        for key, value in self.coefficients.items():
+            L, U = key.split(',')
+            L, U = float(L), float(U)
+            if R >= L and R < U:
+                k = ((np.log10(R) - value['Zlower']) - (value['Zupper'] - np.log10(R))) / (value['Zupper'] - value['Zlower'])
+                for i, c in enumerate(value['coefs']):
+                    T += c * np.cos(i * np.arccos(k))
+        return T
+
+    def get_T_from_tbl(self, R):
+        f = interpolate.interp1d(self.tbl[('Resistance', '(Ohms)')], self.tbl[('Temp.', '(Kelvin)')])
+        return f(R)
+
+    def get_interp_abs_error(self, R):
+        Tc = self.get_T_from_coefs(R)
+        Ti = self.get_T_from_tbl(R)
+        return Tc - Ti
+
+    def get_interp_rel_error(self, R):
+        Tc = self.get_T_from_coefs(R)
+        Ti = self.get_T_from_tbl(R)
+        return (Tc - Ti) / Ti
+
+    def get_deltaT_dat_cof(self):
+        T, DT = [], []
+        for row in self.dat.iterrows():
+            Tdat = row[1][('Temperature','(Kelvin)')]
+            T.append(Tdat)
+            DT.append(Tdat - self.get_T_from_coefs(row[1][('Resistance','(Ohms)')]))
+        return T, DT
+
+    def get_polynomial_fit_uncert(self, T):
+        """ Uncertainty in mK from fitting Chebyshev polynomial """
+        d = {
+        'X93303': [{'Tmin': 1.4 , 'Tmax': 14.1 , 'N': 31, 'n': 9, 'DTrms': 0.93},
+                  {'Tmin': 14.1, 'Tmax': 80.0 , 'N': 31, 'n': 7, 'DTrms': 1.60},
+                  {'Tmin': 80.0, 'Tmax': 325.0, 'N': 32, 'n': 8, 'DTrms': 5.84}],
+        'X115143': [{'Tmin': 20.0, 'Tmax': 95.0, 'N': 28, 'n': 7, 'DTrms': 0.91},
+                   {'Tmin': 95.0, 'Tmax': 325.0, 'N': 29, 'n': 9, 'DTrms': 3.45}],
+        'X115888': [{'Tmin': 20.0, 'Tmax': 95.3, 'N': 28, 'n': 7, 'DTrms': 1.17},
+                   {'Tmin': 95.3, 'Tmax': 325.0, 'N': 29, 'n': 8, 'DTrms': 4.06}]
+        }
+        for _ in d[self.serNum]:
+            if T >= _['Tmin'] and T < _['Tmax']:
+                sigm2 = _['N'] / (_['N'] - _['n']) * _['DTrms']**2
+                break
+            else:
+                sigm2 = 0
+        return 2 * sigm2**0.5
+
+
+class TempUncertainty:
+    sensor = ''
+    def __init__(self, sensor):
+        assert sensor in ['X93303','X115143','X115888']
+        self.sensor = sensor
+        self.cernox = CernoxCal(sensor)
+
+    def _get_R_from_T(self, T):
+        dfx0 = self.cernox.dat.iloc[(self.cernox.dat[('Temperature','(Kelvin)')]-T).abs().argsort()[:1]]
+        res = optimize.minimize(fun = lambda x: np.abs(self.cernox.get_T_from_coefs(x) - T),
+                                x0 = dfx0[('Resistance','(Ohms)')])
+        return res.x[0]
+
+    def get_cernox_uncertainty(self, T):
+        """ Uncertainty in mK for temperature from 20 K to 300 K """
+        return -5e-05 * T**2 + 0.1281 * T + 6.4603
+
+    def get_cabtr_uncertainty(self, T):
+        """
+        Uncertainty in mK for temperature from 20 K to 300 K.
+        Fit to data from CABTR user manual.
+        """
+        return 2e-05 * T**2 + 0.0695 * T - 0.1001
+
+    def get_polynomial_fit_uncert(self, T):
+        """ uncertainty in mK from fitting Chebyshev polynomial """
+        d = {
+        'X93303': [{'Tmin': 1.4 , 'Tmax': 14.1 , 'N': 31, 'n': 9, 'DTrms': 0.93},
+                  {'Tmin': 14.1, 'Tmax': 80.0 , 'N': 31, 'n': 7, 'DTrms': 1.60},
+                  {'Tmin': 80.0, 'Tmax': 325.0, 'N': 32, 'n': 8, 'DTrms': 5.84}],
+        'X115143': [{'Tmin': 20.0, 'Tmax': 95.0, 'N': 28, 'n': 7, 'DTrms': 0.91},
+                   {'Tmin': 95.0, 'Tmax': 325.0, 'N': 29, 'n': 9, 'DTrms': 3.45}],
+        'X115888': [{'Tmin': 20.0, 'Tmax': 95.3, 'N': 28, 'n': 7, 'DTrms': 1.17},
+                   {'Tmin': 95.3, 'Tmax': 325.0, 'N': 29, 'n': 8, 'DTrms': 4.06}]
+        }
+
+        for i in d[self.sensor]:
+            if T >= i['Tmin'] and T < i['Tmax']:
+                sigm2 = i['N'] / (i['N'] - i['n']) * i['DTrms']**2
+                break
+            else:
+                sigm2 = 0
+        return 2 * sigm2**0.5
+
+    def __call__(self, T):
+        """ return combined temperature uncertainty in K """
+        cabtr = self.get_cabtr_uncertainty(T)
+        cernox = self.get_cernox_uncertainty(T)
+        poly = self.get_polynomial_fit_uncert(T)
+        return (cabtr + cernox + poly) * 1e-3
+
+
 class JTCoefficientCalculator:
-    """ Calculate Joule-Thomson coefficients from p_T_pairs data """
-    
     def __init__(self):
-        """ Initialize the calculator with paths and create output directory """
         self.git_root = get_git_root(os.getcwd())
         self.data_dir = os.path.join(self.git_root, 'data', 'derived_data', 'p_T_pairs')
         self.metadata_file = os.path.join(self.git_root, 'data', 'metadata', 'p_T_pairs.json')
         self.output_dir = os.path.join(self.git_root, 'data', 'derived_data', 'JT_coeffs')
-        
-        # Create output directory if it doesn't exist
         os.makedirs(self.output_dir, exist_ok=True)
-        
-        # Load metadata
         self.metadata = self._load_metadata()
         
     def _load_metadata(self):
-        """Load metadata from JSON file."""
-        try:
-            with open(self.metadata_file, 'r') as f:
-                metadata = json.load(f)
-            
-            # Convert to dictionary keyed by filename for easy lookup
-            metadata_dict = {}
-            for item in metadata:
-                metadata_dict[item['filename']] = item
-            
-            return metadata_dict
-        except FileNotFoundError:
-            print(f"Warning: Metadata file not found at {self.metadata_file}")
-            return {}
+        with open(self.metadata_file, 'r') as f:
+            metadata = json.load(f)
+        metadata_dict = {}
+        for item in metadata:
+            metadata_dict[item['filename']] = item
+        return metadata_dict
     
     def _parse_fluid_info(self, filename):
-        """ Parse fluid type and composition from metadata """
-        # Get metadata for this file
         metadata = self.metadata.get(filename, {})
-        
-        if not metadata:
-            print(f"Warning: No metadata found for {filename}")
-            return None, False
-        
         fluid_name = metadata.get('fluid', '')
-        
-        # Determine fluid type and mixture status from metadata
         fluid_type = fluid_name.split("-")
         if len(fluid_type) > 1:
             fluid_type = tuple(fluid_type)
@@ -100,7 +232,7 @@ class JTCoefficientCalculator:
                     best_degree = degree
                     best_fit = poly
         
-                # Stop if R^2 is sufficiently high
+                # Break if R^2 > 0.999
                 if r2 > 0.999:
                     break
                     
@@ -173,36 +305,25 @@ class JTCoefficientCalculator:
         print(f"Processing {filename}...")
         
         try:
-            # Read data
-            df = pd.read_csv(filepath)
-            
-            # Get metadata
-            metadata = self.metadata.get(filename, {})
-            
-            # Parse fluid information from metadata
-            fluid_type, is_mixture = self._parse_fluid_info(filename)
-            
-            if fluid_type is None:
-                print(f"  Skipping {filename} - could not parse fluid info")
-                return None
-            
-            # Extract pressure and temperature data
+            df = pd.read_csv(filepath) # Read data
+            metadata = self.metadata.get(filename, {}) # Get metadata
+            fluid_type, is_mixture = self._parse_fluid_info(filename) # Parse fluid information from metadata
             pressures = df['PT102/MPa'].values # Downstream pressure
             temperatures = df['TT102/K'].values # Downstream temperature
             
-            # Get measurement uncertainties
-            p_uncertainties = df['PT102/MPa_EXP_UNC'].values
-            t_uncertainties = df['TT102/K_EXP_UNC'].values
+            # Get measurement uncertainties (max value of sensor uncertainty and standard expanded uncertainty from a set of measurements)
+            p_uncertainties = np.array([max(p, 1.96 * 0.01/100 * 13.7) for p in df['PT102/MPa_EXP_UNC'].values])
+            comb_T_unc = TempUncertainty("X93303")
+            t_uncertainties = np.array([max(t, comb_T_unc(t)) for t in df['TT102/K_EXP_UNC'].values])
             
             # Skip if insufficient data
             if len(pressures) < 3:
                 print(f"  Insufficient data points: {len(pressures)}")
                 return None
             
-            # Initialize FluidProps
             fluid_props = FluidProps(fluid_type)
             
-            # For mixtures, get composition
+            # Get composition, if mixture
             composition = None
             if is_mixture:
                 if 'x1' in df.columns and not df['x1'].isna().all():
@@ -239,23 +360,24 @@ class JTCoefficientCalculator:
                 'filename': filename,
                 'fluid': fluid_type if isinstance(fluid_type, str) else f"{fluid_type[0]}-{fluid_type[1]}",
                 'is_mixture': is_mixture,
-                'composition': composition,
+                'x1': composition,
+                'x2': None if not is_mixture else (1 - composition),
                 'n_points': len(pressures),
                 'polynomial_degree': degree,
                 'r2_score': r2,
-                'mean_TT101_K': metadata.get('mean_TT101_K', np.nan),
-                'pressures_MPa': pressures,
-                'temperatures_K': temperatures,
-                'pressure_uncertainties_MPa': p_uncertainties,
-                'temperature_uncertainties_K': t_uncertainties,
-                'jt_measured_K_per_MPa': jt_measured,
-                'jt_theoretical_K_per_MPa': jt_theoretical,
-                'jt_uncertainty_K_per_MPa': jt_uncertainty,
-                'relative_abs_error_percent': relative_abs_error,
-                'mean_relative_abs_error_percent': np.nanmean(relative_abs_error),
-                'std_relative_abs_error_percent': np.nanstd(relative_abs_error),
-                'rms_relative_abs_error_percent': np.sqrt(np.nanmean(relative_abs_error**2)),
-                'mean_uncertainty_percent': np.nanmean(jt_uncertainty / np.abs(jt_measured) * 100)
+                'TT101_mean/K': metadata.get('mean_TT101_K', np.nan),
+                'p/MPa': pressures,
+                'T/K': temperatures,
+                'p_UNC/MPa': p_uncertainties,
+                'T_UNC/K': t_uncertainties,
+                'JT_meas/(K/MPa)': jt_measured,
+                'JT_eos/(K/MPa)': jt_theoretical,
+                'JT_UNC/(K/MPa)': jt_uncertainty,
+                'rel_abs_err_perc': relative_abs_error,
+                'mean_rel_abs_err_perc': np.nanmean(relative_abs_error),
+                'std_rel_abs_err_perc': np.nanstd(relative_abs_error),
+                'rms_rel_abs_err_perc': np.sqrt(np.nanmean(relative_abs_error**2)),
+                'mean_unc_perc': np.nanmean(jt_uncertainty / np.abs(jt_measured) * 100)
             }
             
             print(f"  Success: {len(pressures)} points, degree={degree}, R²={r2:.6f}")
@@ -304,15 +426,16 @@ class JTCoefficientCalculator:
                 'filename': result['filename'],
                 'fluid': result['fluid'],
                 'is_mixture': result['is_mixture'],
-                'composition': result['composition'],
-                'mean_TT101_K': result['mean_TT101_K'],
+                'x1': result['x1'],
+                'x2': result['x2'],
+                'TT101_mean/K': result['TT101_mean/K'],
                 'n_points': result['n_points'],
                 'polynomial_degree': result['polynomial_degree'],
                 'r2_score': result['r2_score'],
-                'mean_relative_abs_error_percent': result['mean_relative_abs_error_percent'],
-                'std_relative_abs_error_percent': result['std_relative_abs_error_percent'],
-                'rms_relative_abs_error_percent': result['rms_relative_abs_error_percent'],
-                'mean_uncertainty_percent': result['mean_uncertainty_percent']
+                'mean_rel_abs_err_perc': result['mean_rel_abs_err_perc'],
+                'std_rel_abs_err_perc': result['std_rel_abs_err_perc'],
+                'rms_rel_abs_err_perc': result['rms_rel_abs_err_perc'],
+                'mean_unc_perc': result['mean_unc_perc']
             })
         
         # Save summary as CSV
@@ -340,14 +463,14 @@ class JTCoefficientCalculator:
         # Save individual CSV files for each measurement
         for result in results:
             csv_data = {
-                'pressure_MPa': result['pressures_MPa'],
-                'temperature_K': result['temperatures_K'],
-                'pressure_uncertainty_MPa': result['pressure_uncertainties_MPa'],
-                'temperature_uncertainty_K': result['temperature_uncertainties_K'],
-                'jt_measured_K_per_MPa': result['jt_measured_K_per_MPa'],
-                'jt_theoretical_K_per_MPa': result['jt_theoretical_K_per_MPa'],
-                'jt_uncertainty_K_per_MPa': result['jt_uncertainty_K_per_MPa'],
-                'relative_abs_error_percent': result['relative_abs_error_percent']
+                'p/MPa': result['p/MPa'],
+                'T/K': result['T/K'],
+                'p_UNC/MPa': result['p_UNC/MPa'],
+                'T_UNC/K': result['T_UNC/K'],
+                'JT_meas/(K/MPa)': result['JT_meas/(K/MPa)'],
+                'JT_eos/(K/MPa)': result['JT_eos/(K/MPa)'],
+                'JT_UNC/(K/MPa)': result['JT_UNC/(K/MPa)'],
+                'mean_rel_abs_err_perc': result['mean_rel_abs_err_perc']
             }
             
             csv_df = pd.DataFrame(csv_data)
